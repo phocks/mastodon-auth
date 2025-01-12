6d1834dc048f6bf477aa8967fd5dc856e01bcb8d
Joshua Byrd <phocks@gmail.com>
2025-01-12 18:21:39 +1000

    Add moduledocs?

--- mod.ts
+++ mod.ts
@@ -1,2 +1,8 @@
+/**
+* This module contains functions to do auth on
+* Mastodon Instances.
+* @module
+*/
+
 export { getToken, getAuthUrl } from "./auth.ts";
 export type { TokenResponse, AccountToken } from "./types.ts";
